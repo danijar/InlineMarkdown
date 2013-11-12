3420f46481434ae39fc854efd04658a53b82e944
Danijar Hafner <mail@danijar.org>
2013-11-12 14:59:47 +0100

    improved cursor
    last issue is with line breaks

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ $(document).ready(function() {
 	var cursor = 0;
 
 	var types = {
-		bold:   { regex: /\*\*[^\*\s]([^\*]|(\*\s))*[^\*\s]\*\*/g, callback: function(match){ return '<b>' + match + '</b>' } },
-		italic: { regex: /\*[^\*\s]([^\*]|(\*\s))*[^\*\s]\*/g,     callback: function(match){ return '<i>' + match + '</i>' } },
-		h1: { regex: /^#{1}([^#]|$).*$/mg, callback: function(match){ return '<h1>' + match + '</h1>' } },
-		h2: { regex: /^#{2}([^#]|$).*$/mg, callback: function(match){ return '<h2>' + match + '</h2>' } },
-		h3: { regex: /^#{3}([^#]|$).*$/mg, callback: function(match){ return '<h3>' + match + '</h3>' } },
-		h4: { regex: /^#{4}([^#]|$).*$/mg, callback: function(match){ return '<h4>' + match + '</h4>' } },
-		h5: { regex: /^#{5}([^#]|$).*$/mg, callback: function(match){ return '<h5>' + match + '</h5>' } },
-		h6: { regex: /^#{6}([^#]|$).*$/mg, callback: function(match){ return '<h6>' + match + '</h6>' } },
+		bold:   { regex: /\*\*[^\*\s]([^\*]|(\*\s))*[^\*\s]\*\*/g, before: '<b>', after: '</b>' },
+		italic: { regex: /\*[^\*\s]([^\*]|(\*\s))*[^\*\s]\*/g, before: '<i>', after: '</i>' },
+		h1: { regex: /^#{1}([^#]|$).*$/mg, before: '<h1>', after: '</h1>' },
+		h2: { regex: /^#{2}([^#]|$).*$/mg, before: '<h2>', after: '</h2>' },
+		h3: { regex: /^#{3}([^#]|$).*$/mg, before: '<h3>', after: '</h3>' },
+		h4: { regex: /^#{4}([^#]|$).*$/mg, before: '<h4>', after: '</h4>' },
+		h5: { regex: /^#{5}([^#]|$).*$/mg, before: '<h5>', after: '</h5>' },
+		h6: { regex: /^#{6}([^#]|$).*$/mg, before: '<h6>', after: '</h6>' },
 	};
 
 	$(document).keypress(function(e) {
@@ -31,8 +31,9 @@ $(document).ready(function() {
 		fetch();
 		format();
 		render();
+		cursor_insert();
 		output();
-		blink();
+		cursor_display();
 	}
 
 	update();
@@ -41,7 +42,7 @@ $(document).ready(function() {
 		var char = e.charCode ? String.fromCharCode(e.charCode) : undefined;
 		if(e.keyCode)
 		{
-			//console.log(e.keyCode);
+			console.log(e.keyCode);
 			switch(e.keyCode)
 			{
 			case  8: // backspace
@@ -94,18 +95,38 @@ $(document).ready(function() {
 
 	function format() {
 		list = plain;
-		indices  = _.range(0, list.length);
+		indices = _.range(0, list.length);
 
 		_.each(features, function(feature) {
 			if(feature.type in types) {
 				var type = types[feature.type];
-				var string = plain.substring(feature.from, feature.to + 1);
-				replace(feature.from, feature.to, type.callback(string));
+				console.log(type);
+				implace(feature.from, type.before);
+				implace(feature.to + 1, type.after);
+				//var string = plain.substring(feature.from, feature.to + 1);
+				//replace(feature.from, feature.to, type.callback(string));
 			}
 		});
 
 		//console.log(list);
 		//console.log(indices);
+	}
+
+	function implace(position, string) {
+		var index;
+		for(var i = 0; i < indices.length; ++i)
+			if(indices[i] == position)
+				index = i;
+
+		if(index === undefined) { console.log("cannot insert"); return; }
+		
+		var start = list.length;
+		var end = start + string.length;
+		list += string;
+
+		var spliced = indices.splice.apply(indices, [index, 0].concat(_.range(start, end)));
+
+		//console.log("from " + from + " to " + to + " spliced " + spliced);
 	}
 
 	function replace(plain_from, plain_to, string) {
@@ -130,23 +151,29 @@ $(document).ready(function() {
 
 	function render() {
 		
-		var index, curpos;
-		for(var i = 0; i < indices.length; ++i)
-			if(indices[i] == cursor)
-				index = i;
-
 		rendered = '';
-		for(var i = 0; i < indices.length; ++i) {
-			if(index == i)
-				curpos = rendered.length;
+		for(var i = 0; i < indices.length; ++i)
 			rendered += list[indices[i]];
-		}
-
-		if(curpos === undefined) {console.log("cursor not found"); curpos = rendered.length; }
-		rendered = insert(rendered, curpos, '<span class="curpos" at="' + cursor + '"></span>');
 
 		rendered = rendered.replace(/\n/g, '<br>');
 		//rendered = rendered.replace(' ', '&nbsp;'); // only multiple whitespaces and outside of tags
+	}
+
+	function cursor_insert()
+	{
+		var index;
+		if(cursor == plain.length)
+			index = rendered.length;
+		else
+			for(var i = 0; i < rendered.length; ++i)
+				if(indices[i] == cursor)
+					index = i;
+
+		console.log("text   plain " + plain.length + " formatted " + rendered.length);
+		console.log("cursor plain " + cursor + " formatted " + index);
+
+		if(index === undefined) { console.log("cursor not found"); index = rendered.length; }
+		rendered = insert(rendered, index, '<span class="curpos" at="' + cursor + '"></span>');
 	}
 
 	function insert(into, at, string)
@@ -154,11 +181,7 @@ $(document).ready(function() {
 		return into.substring(0, at) + string + into.substring(at);
 	}
 
-	function output() {
-		$('.sheet.real').html(rendered == '' ? '<gray>...</gray>' : rendered);
-	}
-
-	function blink()
+	function cursor_display()
 	{
 		var streak = $('.cursor');
 		if(rendered == '') {streak.hide(); return; }
@@ -167,6 +190,10 @@ $(document).ready(function() {
 		var top = offset.top - streak.outerHeight() * 0.8, left = offset.left;
 
 		streak.show().css({ 'top': top + 'px', 'left': left + 'px' });
+	}
+
+	function output() {
+		$('.sheet.real').html(rendered == '' ? '<gray>...</gray>' : rendered);
 	}
 
 });
